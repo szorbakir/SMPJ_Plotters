f15fa78ef4fe1684360650d8cde87fbecc161825
İbrahim Soner Zorbakır <zorbakir@zorbakir-mac.dyndns.cern.ch>
2017-08-23 16:27:43 +0200

    modified for 2016 whole data and new MCs

--- smp_plotter_pt.cpp
+++ smp_plotter_pt.cpp
@@ -7,12 +7,16 @@ void smp_plotter_pt(){
     
     TString observable[] = {
         "hpt",
+        "hdj_leading",
+        "hdj_subleading",
         "hpt1",
         "hpt2"
     };
     
     TString axis_name[] = {
         "Raw p_{T} Spectrum (GeV)",
+        "Leading Jet p_{T} (GeV)",
+        "Subleading Jet p_{T} (GeV)",
         "Leading Jet p_{T} (GeV)",
         "Subleading Jet p_{T} (GeV)"
     };
@@ -32,63 +36,73 @@ void smp_plotter_pt(){
     TString inputFile1;
     TString inputFile2;
     TString inputFile3;
+    TString inputFile4;
     
     Double_t w = 1200;
-    Double_t h = 900;
+    Double_t h = 800;
     Double_t norm = 1;
     
     TCanvas * c1 = new TCanvas("c1", "c1", w, h);
     
-    TH1F *h_MC_;
+    TH1D *h_MadGraph_;
+    TH1D *h_Herwig_;
+    TH1D *h_Pythia_;
     
-    TH1F *h_part1_DATA_;
-    TH1F *h_part2_DATA_;
+    TH1D *h_DATA_;
     
-    TH1F *h_Ratio;
-    TH1F *h_Ratio_2;
+    TH1D *h_Ratio_MD_;
+    TH1D *h_Ratio_HW_;
+    TH1D *h_Ratio_PYT_;
     
-    inputFile1.Form("output-MC-2b.root");
-    inputFile2.Form("output-RunBtoFearly-2b.root");
-    inputFile3.Form("output-RunFlatetoH-2b.root");
+    
+    inputFile1.Form("output-MDG-2b.root");
+    inputFile2.Form("output-HW-2b.root");
+    inputFile3.Form("output-MC-2a.root");
+    inputFile4.Form("output-DATA-2b.root");
     
     int counter = (sizeof(observable)/sizeof(*observable));
     int bin_counter = (sizeof(eta_bins)/sizeof(*eta_bins));
     
-    
-    for (int j=0; j<bin_counter-1; j++) { 
+    for (int j=0; j<bin_counter-1; j++) {
         for (int i=0; i<counter; i++) {
         
-            TString hname = (TString) (observable[i]+"_"+eta_bins[j] + "-" + eta_bins[j+1]); //Kisaltmalari ogren ve ona gore string array olustur...
+            TString hname = (TString) (observable[i]+"_"+eta_bins[j] + "-" + eta_bins[j+1]);
             TString eta_name = (TString) (eta_bins[j] + " < #eta < " + eta_bins[j+1]);
             
             //MC
-            TFile* MCFile = TFile::Open(inputFile1.Data(), "READ");
-            MCFile -> cd("Standard/Eta_" + eta_bins[j] + "-" + eta_bins[j+1]);
-            h_MC_ = (TH1F*)gDirectory->FindObjectAny(observable[i]);
+            TFile* MCFile1 = TFile::Open(inputFile1.Data(), "READ");
+            MCFile1 -> cd("Standard/Eta_" + eta_bins[j] + "-" + eta_bins[j+1]);
+            h_MadGraph_ = (TH1D*)gDirectory->FindObjectAny(observable[i]);
+            
+            //MC
+            TFile* MCFile2 = TFile::Open(inputFile2.Data(), "READ");
+            MCFile2 -> cd("Standard/Eta_" + eta_bins[j] + "-" + eta_bins[j+1]);
+            h_Herwig_ = (TH1D*)gDirectory->FindObjectAny(observable[i]);
+            
+            //MC
+            TFile* MCFile3 = TFile::Open(inputFile3.Data(), "READ");
+            MCFile3 -> cd("Standard/Eta_" + eta_bins[j] + "-" + eta_bins[j+1]);
+            h_Pythia_ = (TH1D*)gDirectory->FindObjectAny(observable[i]);
     
             //DATA
-            TFile* DATAFile = TFile::Open(inputFile2.Data(), "READ");
+            TFile* DATAFile = TFile::Open(inputFile4.Data(), "READ");
             DATAFile -> cd("Standard/Eta_" + eta_bins[j] + "-" + eta_bins[j+1]);
-            h_part1_DATA_ = (TH1F*)gDirectory->FindObjectAny(observable[i]);
-            
-            TFile* DATAFile2 = TFile::Open(inputFile3.Data(), "READ");
-            DATAFile2 -> cd("Standard/Eta_" + eta_bins[j] + "-" + eta_bins[j+1]);
-            h_part2_DATA_ = (TH1F*)gDirectory->FindObjectAny(observable[i]);
+            h_DATA_ = (TH1D*)gDirectory->FindObjectAny(observable[i]);
             
             double xmin = 1000000, xmax = 0;
             double ymin = 0.;
             double ymax = 0.;
 
-            int ncells = h_part1_DATA_->GetSize();  // it adds underflow and overflow bins so I have to extract those
+            int ncells = h_DATA_->GetSize();  // it adds underflow and overflow bins so I have to extract those
             int Data_NBins = ncells-2;
             
             for (int k=1; k<=Data_NBins; k++) {
                 
-                double check_bins = h_part1_DATA_->GetBinContent(k);
+                double check_bins = h_DATA_->GetBinContent(k);
                 
                 if (check_bins!=0) {
                     
-                    double Xaxis_data = h_part1_DATA_->GetBinCenter(k);
+                    double Xaxis_data = h_DATA_->GetBinCenter(k);
                     
                     if (k < xmin) {xmin = k; ymin = Xaxis_data;}
                     if (k > xmax) {xmax = k; ymax = Xaxis_data;}
@@ -96,16 +110,21 @@ void smp_plotter_pt(){
             }
             
             // Basic Scaling ////
-            double Int_data = (h_part1_DATA_->Integral());
-            double Int_MC   = (h_MC_->Integral(xmin,xmax)); //Numbers are coming from the loop above...They corresponds to first and last bin of data///
-            double Int_data_2 = (h_part2_DATA_->Integral(xmin,xmax));
+            double Int_data = (h_DATA_->Integral());
+            double Int_MC1   = (h_MadGraph_->Integral(xmin,xmax));
+            double Int_MC2   = (h_Herwig_->Integral(xmin,xmax));
+            double Int_MC3   = (h_Pythia_->Integral(xmin,xmax));
             
             double Scale_Factor_1 = 0;
             double Scale_Factor_2 = 0;
-            Scale_Factor_1 = Int_data/Int_MC;
-            h_MC_->Scale(Scale_Factor_1);
-            Scale_Factor_2 = Int_data / Int_data_2;
-            h_part2_DATA_ -> Scale(Scale_Factor_2);
+            double Scale_Factor_3 = 0;
+            
+            Scale_Factor_1 = Int_data/Int_MC1;
+            h_MadGraph_->Scale(Scale_Factor_1);
+            Scale_Factor_2 = Int_data/Int_MC2;
+            h_Herwig_->Scale(Scale_Factor_2);
+            Scale_Factor_3 = Int_data/Int_MC3;
+            h_Pythia_->Scale(Scale_Factor_3);
             //End of scaling //
             
             //Draw, cosmetics, etc.
@@ -121,46 +140,52 @@ void smp_plotter_pt(){
             pad1-> SetLogx();
             pad1->cd();
     
-            h_Ratio = (TH1F*)h_MC_->Clone("h_Ratio");
-            h_Ratio_2 = (TH1F*)h_MC_->Clone("h_Ratio_2");
+            h_Ratio_MD_ = (TH1D*) h_MadGraph_->Clone("h_Ratio_MD_");
+            h_Ratio_HW_ = (TH1D*) h_Herwig_->Clone("h_Ratio_HW_");
+            h_Ratio_PYT_ = (TH1D*) h_Pythia_->Clone("h_Ratio_PYT_");
 
-            h_MC_->GetXaxis()->SetRangeUser(ymin,2500);
-            h_MC_->GetXaxis()->SetLabelOffset(999);
-            h_MC_->GetYaxis()->SetNdivisions(505);
-            h_MC_->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-            h_MC_->GetXaxis()->SetLabelSize(20);
-            h_MC_->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-            h_MC_->GetYaxis()->SetLabelSize(20);
-            h_MC_->SetMarkerColor(kBlack);
-            h_MC_->SetLineColor(kBlack);
-            h_MC_->SetLineWidth(1);
-            h_MC_->SetYTitle("A.U.");
-            h_MC_->GetYaxis()->SetTitleOffset(0.8);
-            h_MC_->GetYaxis()->SetTitleSize(0.05);
-            h_MC_->GetYaxis()->SetTitleFont(42);
+            h_MadGraph_->GetXaxis()->SetRangeUser(ymin,2500);
+            h_MadGraph_->GetXaxis()->SetLabelOffset(999);
+            h_MadGraph_->GetYaxis()->SetNdivisions(505);
+            h_MadGraph_->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+            h_MadGraph_->GetXaxis()->SetLabelSize(20);
+            h_MadGraph_->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+            h_MadGraph_->GetYaxis()->SetLabelSize(20);
+            h_MadGraph_->SetMarkerColor(kBlue+2);
+            h_MadGraph_->SetLineColor(kBlue+2);
+            h_MadGraph_->SetLineWidth(2);
+            h_MadGraph_->SetYTitle("A.U.");
+            h_MadGraph_->GetYaxis()->SetTitleOffset(0.8);
+            h_MadGraph_->GetYaxis()->SetTitleSize(0.05);
+            h_MadGraph_->GetYaxis()->SetTitleFont(42);
+            
+            h_Herwig_->SetMarkerColor(kGreen+2);
+            h_Herwig_->SetLineColor(kGreen+2);
+            h_Herwig_->SetLineWidth(2);
+            
+            h_Pythia_->SetMarkerColor(kRed+2);
+            h_Pythia_->SetLineColor(kRed+2);
+            h_Pythia_->SetLineWidth(2);
+            
 
-            h_part1_DATA_->SetMarkerColor(kBlue);
-            h_part1_DATA_->SetMarkerStyle(20);
-            h_part1_DATA_->SetLineColor(kBlue);
-            h_part1_DATA_->SetLineWidth(1);
+            h_DATA_->SetMarkerColor(kBlack);
+            h_DATA_->SetMarkerStyle(20);
+            h_DATA_->SetLineColor(kBlack);
+            h_DATA_->SetLineWidth(2);
             
-            h_part2_DATA_->SetMarkerColor(kRed);
-            h_part2_DATA_->SetMarkerStyle(20);
-            h_part2_DATA_->SetLineColor(kRed);
-            h_part2_DATA_->SetLineWidth(1);
+            h_MadGraph_->Draw("E1HIST");
+            h_Herwig_->Draw("E1HISTSAME");
+            h_Pythia_->Draw("E1HISTSAME");
+            h_DATA_->Draw("E1HIST SAME");
             
-            h_MC_->Draw("E1HIST");
-            h_part1_DATA_->Draw("E1HIST SAME");
-            h_part2_DATA_->Draw("E1HIST SAME");
-            
-    
-            TLegend *leg = new TLegend(0.6060601,0.6689862,0.7079967,0.8901843,NULL,"brNDC");
+            TLegend *leg = new TLegend(0.7412354,0.6239631,0.8430718,0.8875576,NULL,"brNDC");
             leg->SetBorderSize(0);
             leg->SetTextFont(42);
             leg->SetTextSize(0.045);
-            leg->AddEntry(h_MC_,"MC","l");
-            leg->AddEntry(h_part1_DATA_,"RunBCDEF_{Early}, 19.49 fb^{-1}","lep");
-            leg->AddEntry(h_part2_DATA_,"RunF_{Late}GH,  16.5 fb^{-1}","lep");
+            leg->AddEntry(h_MadGraph_,"MadGraph","l");
+            leg->AddEntry(h_Herwig_,"Herwig","l");
+            leg->AddEntry(h_Pythia_,"Pythia","l");
+            leg->AddEntry(h_DATA_,"RunBCDEFGH, ","lp");
             leg->AddEntry("","AK4chs Jets","");
             leg->AddEntry("",eta_name,"");
             leg->Draw();
@@ -170,7 +195,7 @@ void smp_plotter_pt(){
             t6->SetTextFont(42);
     	    t6->SetTextSize(0.05);
     	    t6->SetTextAlign(11);
-            t6->DrawLatex(0.1,0.92,"CMS Internal, #sqrt{s}=13 TeV");
+            t6->DrawLatex(0.1,0.92,"#bf{CMS} Internal, #sqrt{s}=13 TeV, 39.9 fb^{-1}");
             
             c1->cd();          // Go back to the main canvas before defining pad2
    	
@@ -182,41 +207,48 @@ void smp_plotter_pt(){
             pad2->cd();       // pad2 becomes the current pad
             gPad-> SetLogx();
     
-            h_Ratio->Divide(h_part1_DATA_);
-            h_Ratio_2->Divide(h_part2_DATA_);
-            
-            //cosmetics
-            h_Ratio->GetXaxis()->SetRangeUser(ymin,2500);
-            h_Ratio->SetXTitle(axis_name[i]);
-            h_Ratio->SetYTitle("MC/Data");
-            h_Ratio->GetYaxis()->SetTitleOffset(0.8);
-            h_Ratio->GetYaxis()->CenterTitle();
-            h_Ratio->GetYaxis()->SetNdivisions(511);
-            h_Ratio->SetMarkerColor(kBlue);
-            h_Ratio->SetLineColor(kBlue);
-            h_Ratio->SetLineWidth(1);
-            h_Ratio->GetYaxis()->SetRangeUser(0.5,1.5);
-            h_Ratio->GetYaxis()->SetNdivisions(505);
-            h_Ratio->GetYaxis()->SetTitleSize(0.11);
-            h_Ratio->GetYaxis()->SetTitleFont(42);
-            h_Ratio->GetYaxis()->SetTitleOffset(0.35);
-            h_Ratio->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-            h_Ratio->GetYaxis()->SetLabelSize(18);
-            h_Ratio->GetXaxis()->SetMoreLogLabels(); //to make the x-axis a bit easier to read and see where the axis starts.
-    	    h_Ratio->GetXaxis()->SetNoExponent(); //to make the x-axis a bit easier to read and see where the axis starts.
-            h_Ratio->GetXaxis()->SetTitleSize(0.11);
-            h_Ratio->GetXaxis()->SetTitleFont(42);
-            h_Ratio->GetXaxis()->SetTitleOffset(1.1);
-            h_Ratio->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-            h_Ratio->GetXaxis()->SetLabelSize(18);
-           
-            h_Ratio_2->SetMarkerColor(kRed);
-            h_Ratio_2->SetLineColor(kRed);
-            h_Ratio_2->SetLineWidth(1);
-
-            h_Ratio->Draw("E1HIST");
-            h_Ratio_2->Draw("E1HIST SAME");
+            h_Ratio_MD_->Divide(h_DATA_);
+            h_Ratio_HW_->Divide(h_DATA_);
+            h_Ratio_PYT_->Divide(h_DATA_);
     
+            //cosmetics
+            h_Ratio_MD_->GetXaxis()->SetRangeUser(ymin,2500);
+            h_Ratio_MD_->SetXTitle(axis_name[i]);
+            h_Ratio_MD_->SetYTitle("MC/Data");
+            h_Ratio_MD_->GetYaxis()->SetTitleOffset(0.8);
+            h_Ratio_MD_->GetYaxis()->CenterTitle();
+            h_Ratio_MD_->SetMarkerColor(kBlue+2);
+            h_Ratio_MD_->SetLineColor(kBlue+2);
+            h_Ratio_MD_->SetLineWidth(2);
+            h_Ratio_MD_->GetYaxis()->SetRangeUser(0.5,1.9);
+            h_Ratio_MD_->GetYaxis()->SetNdivisions(606);
+            h_Ratio_MD_->GetYaxis()->SetTitleSize(0.11);
+            h_Ratio_MD_->GetYaxis()->SetTitleFont(42);
+            h_Ratio_MD_->GetYaxis()->SetTitleOffset(0.35);
+            h_Ratio_MD_->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+            h_Ratio_MD_->GetYaxis()->SetLabelSize(18);
+            h_Ratio_MD_->GetXaxis()->SetMoreLogLabels(); //to make the x-axis a bit easier to read and see where the axis starts.
+    	    h_Ratio_MD_->GetXaxis()->SetNoExponent(); //to make the x-axis a bit easier to read and see where the axis starts.
+            h_Ratio_MD_->GetXaxis()->SetTitleSize(0.11);
+            h_Ratio_MD_->GetXaxis()->SetTitleFont(42);
+            h_Ratio_MD_->GetXaxis()->SetTitleOffset(1.1);
+            h_Ratio_MD_->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+            h_Ratio_MD_->GetXaxis()->SetLabelSize(18);
+            
+            h_Ratio_HW_->SetMarkerColor(kGreen+2);
+            h_Ratio_HW_->SetLineColor(kGreen+2);
+            h_Ratio_HW_->SetLineWidth(2);
+            
+            h_Ratio_PYT_->SetMarkerColor(kRed+2);
+            h_Ratio_PYT_->SetLineColor(kRed+2);
+            h_Ratio_PYT_->SetLineWidth(2);
+            
+           
+
+            h_Ratio_MD_->Draw("E1HIST");
+            h_Ratio_HW_->Draw("E1HISTSAME");
+            h_Ratio_PYT_->Draw("E1HISTSAME");
+            
             c1->SaveAs(hname + ".png"); //saving...
         }
     }
